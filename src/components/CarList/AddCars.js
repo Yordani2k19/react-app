dae296537f0628f5ebe9916ca14656341eda704c
yordani2k19 <yordani@lit.technology>
2020-07-24 22:37:52 -0400

    Final commit

--- src/components/CarList/AddCars.js
+++ src/components/CarList/AddCars.js
@@ -1,36 +1,32 @@
-import React from "react";
+import React from 'react'
 
 class AddCars extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       id: null,
-      make: "",
-      model: "",
-      year: "",
+      make: '',
+      model: '',
+      year: '',
       ...props.carBeingEdited
-    };
+    }
   }
 
-  makeEvent = e => this.setState({ make: e.target.value });
-  // Car Make Event
-  modelEvent = e => this.setState({ model: e.target.value });
-  // Car Model Event
-  yearEvent = e => this.setState({ year: e.target.value });
-  // Car Year Event
+  makeEvent = e => this.setState({ make: e.target.value })
+  modelEvent = e => this.setState({ model: e.target.value })
+  yearEvent = e => this.setState({ year: e.target.value })
 
-  // Submit Event
   carFormSubmit = e => {
-    e.preventDefault();
-    const { id } = this.state;
-    const { addCar, updateCar } = this.props;
-    id ? updateCar(this.state) : addCar(this.state);
+    e.preventDefault()
+    const { id } = this.state
+    const { addCar, updateCar } = this.props
+    id ? updateCar(this.state) : addCar(this.state)
 
-    this.setState({ make: "" });
-    this.setState({ model: "" });
-    this.setState({ year: "" });
-  };
+    this.setState({ make: '' })
+    this.setState({ model: '' })
+    this.setState({ year: '' })
+  }
 
   render() {
     return (
@@ -66,59 +62,8 @@ class AddCars extends React.Component {
           <input type="submit" value="Submit!" id="submitCarBtn" />
         </form>
       </div>
-    );
+    )
   }
 }
 
-export default AddCars;
-
-// -----APPjs-----
-// import Cars from "./components/CarList/Cars";
-// import AddCars from "./components/CarList/AddCars";
-
-// class App extends React.Component {
-//   state = {
-//     carList: []
-//   };
-
-//   addCar = car => {
-//     const newCar = {
-//       ...car,
-//       id: uuidv4()
-//     };
-//     this.setState({
-//       carList: [...this.state.carList, newCar]
-//     });
-//   };
-
-//   delCar = id => {
-//     this.setState({
-//       carList: [...this.state.carList.filter(deleteCar => deleteCar.id !== id)]
-//     });
-//   };
-
-//   updateCar = updatedCar => {
-//     this.setState({
-//       carList: [
-//         ...this.state.carList.map(car =>
-//           car.id === updatedCar.id ? updatedCar : car
-//         )
-//       ]
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <Cars
-//           carList={this.state.carList}
-//           delCar={this.delCar}
-//           updateCar={this.updateCar}
-//         />
-//         <AddCars addCar={this.addCar} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
+export default AddCars
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from "./App";
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById("root_1"));
-
-//https://www.youtube.com/watch?v=DLX62G4lc44&feature=youtu.be
-//24:02
+ReactDOM.render(<App />, document.getElementById('root_1'))
